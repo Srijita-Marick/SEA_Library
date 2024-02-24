e4e5bf8c343b516f46cf92f94652440055aa5ae2
emily <emily.ngkwongsang@ucalgary.ca>
2024-02-24 14:39:44 -0700

    Updating storeNewBook and adding checkExistBook function

--- src/Data.java
+++ src/Data.java
@@ -1,7 +1,13 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Data {
     private static final ArrayList<Object[]> books = new ArrayList<>();
+
+    private static final ArrayList<String> titles = new ArrayList<>();
+
+    private static final ArrayList<String> authors = new ArrayList<>();
     public static final int INDEX_TITLE = 0;
     public static final int INDEX_AUTHOR = 1;
     public static final int INDEX_GENRE = 2;
@@ -9,15 +15,26 @@ public class Data {
     //public static final int INDEX_DATEDUE = 4;
     public static final int INDEX_REMOVED = 4;
     public static boolean storeNewBook(String title, String author, String genre, String availabilityStatus) {
-        Object[] book = new Object[6];
-        book[INDEX_TITLE] = title;
-        book[INDEX_AUTHOR] = author;
-        book[INDEX_GENRE] = genre;
-        book[INDEX_STATUS] = availabilityStatus;
-        //book[INDEX_DATEDUE] = dateDue;
-        book[INDEX_REMOVED] = false;
-        books.add(book);
-        System.out.println("Stored Yay!");
-        return true;
+        if (!checkExistBook(title, author)) {
+            Object[] book = new Object[6];
+            book[INDEX_TITLE] = title;
+            book[INDEX_AUTHOR] = author;
+            book[INDEX_GENRE] = genre;
+            book[INDEX_STATUS] = availabilityStatus;
+            //book[INDEX_DATEDUE] = dateDue;
+            book[INDEX_REMOVED] = false;
+            books.add(book);
+            titles.add(title);
+            authors.add(author);
+            System.out.println("Stored Yay!");
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    private static boolean checkExistBook(String title , String author){
+        return titles.contains(title) || authors.contains(author);
+    }
+
 }
