d245a451bcc67d59aba0dce4c9deb02a0bf74ed4
Srijita Marick <srijita.marick@ucalgary.ca>
2024-02-25 16:07:19 -0700

    Created file DataTest.java

---
+++ src/DataTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTest {
+
+    @org.junit.jupiter.api.Test
+    void storeNewBook() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllBooks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAvailableBooks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUnavailableBooks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void storeNewMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBorrowedBooks() {
+    }
+}
