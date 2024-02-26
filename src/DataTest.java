cd15e5848077dcd0b3004bcaa48f0f8f4e5b6f44
emily <emily.ngkwongsang@ucalgary.ca>
2024-02-26 01:10:30 -0700

    added reset() for better testing and 1 JUnit test created for storeNewBook

--- src/DataTest.java
+++ src/DataTest.java
@@ -3,9 +3,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
-    @Test
+    @org.junit.jupiter.api.Test
     void testStoreNewBook() {
+        Data.reset();
+        String title = "Harry Potter and the Sorcerer's Stone";
+        String author = "J.K. Rowling";
+        String genre = "Fantasy";
+        String availabilityStatus = "Available";
+        assertEquals(0, Data.getAllBooks().size());
+        boolean success = Data.storeNewBook(title, author, genre, availabilityStatus);
+        assertEquals(1, Data.getAllBooks().size());
+        assertEquals(title, Data.getAllBooks().get(0)[0]);
+        assertEquals(author, Data.getAllBooks().get(0)[1]);
+        assertEquals(genre, Data.getAllBooks().get(0)[2]);
+        assertEquals(availabilityStatus, Data.getAllBooks().get(0)[3]);
+        assertTrue(success, "Add was successful");
     }
+
 
     @Test
     void checkExistBook() {
