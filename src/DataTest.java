aa47b31ab50f1e65fde1c3cdafffba1765c16be5
emily <emily.ngkwongsang@ucalgary.ca>
2024-02-26 16:46:28 -0700

    added checkBookAvailableTitleWithNullAuthor test , edited checkBookAvailable test

--- src/DataTest.java
+++ src/DataTest.java
@@ -155,13 +155,19 @@ class DataTest {
     }
     @Test
     void checkBookAvailable() {
-        assertTrue(Data.checkBookAvailable("Joyland", "Emily Schultz"));
+        assertTrue(Data.checkBookAvailable("Hell Bent","Leigh Bardugo"));
     }
 
     @Test
     void checkBookUnavailable(){
         assertTrue(Data.checkBookAvailable("In the Lives of Puppets","T.J. Klune"));
     }
+
+    @Test
+    void checkBookAvailableTitleWithNullAuthor(){
+        assertFalse(Data.checkBookAvailable("Joyland",null));
+    }
+
 
     @Test
     void testGetAllBooks() {
