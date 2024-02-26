e63ef4d4c69cf86ee99ba2b74c1573fc7be6e447
Srijita Marick <srijita.marick@ucalgary.ca>
2024-02-26 15:43:48 -0700

    got rid of unnecessary import statements

--- src/DataTest.java
+++ src/DataTest.java
@@ -1,11 +1,6 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
