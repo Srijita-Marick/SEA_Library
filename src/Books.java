d13be91e9f41a4fc5fb424a6a26442fe324266a2
Srijita Marick <srijita.marick@ucalgary.ca>
2024-03-16 13:31:56 -0600

    Created getters for Books

--- src/Books.java
+++ src/Books.java
@@ -7,4 +7,24 @@ public class Books {
         this.availabilityStatus = availabilityStatus;
         this.type = type;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
