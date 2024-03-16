a155a7c58649b817cefca9566dc5670d7c86dae9
Srijita Marick <srijita.marick@ucalgary.ca>
2024-03-16 13:28:56 -0600

    Initial Setup for OOP structure in Books

--- src/Books.java
+++ src/Books.java
@@ -1,18 +1,10 @@
 public class Books {
-    public static final int INDEX_TITLE = 0;
-    public static final int INDEX_AUTHOR = 1;
-    public static final int INDEX_GENRE = 2;
-    public static final int INDEX_STATUS = 3;
-    public static final int INDEX_TYPE = 4;
-    public static final int INDEX_REMOVED = 5;
-    private Object[] book;
+    private String title, author, genre, availabilityStatus, type;
     public Books(String title, String author, String genre, String availabilityStatus, String type){
-        book = new Object[5];
-        book[INDEX_TITLE] = title;
-        book[INDEX_AUTHOR] = author;
-        book[INDEX_GENRE] = genre;
-        book[INDEX_STATUS] = availabilityStatus;
-        book[INDEX_TYPE] = type;
-        book[INDEX_REMOVED] = false;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.availabilityStatus = availabilityStatus;
+        this.type = type;
     }
 }
