d0f7705eb839fa2cdcb0d8dae7d79ded1c589187
Anna Littkemann <anna.littkemann@ucalgary.ca>
2024-03-18 17:36:08 -0600

    reformatted toString methods

--- src/LibraryProjectPackage/objects/AdultMember.java
+++ src/LibraryProjectPackage/objects/AdultMember.java
@@ -16,10 +16,16 @@ public class AdultMember extends Member {
     }
 
     public String toString() {
-        return (this.getID() + "\t" +
-                this.getName() + '\t' +
-                "Fines: " + this.getFines() + "\n" +
-                "---------------" +
-                "LibraryProjectPackage.objects.Books borrowed: \n" + this.getBorrowed() + "\n");
+        StringBuilder memString = new StringBuilder();
+        memString.append("\n--------------------");
+        memString.append("\nID: ").append(this.getID());
+        memString.append("\nName: ").append(this.getName());
+        memString.append("\nBooks Borrowed:");
+        for (String book: LibraryProjectPackage.Data.getBorrowedBooks(this.getID())){
+            memString.append("\n     ").append(book);
+        }
+        String roundedFine = String.format("%.2f",(this.getFines()));
+        memString.append("\nFines: ").append(roundedFine);
+        return memString.toString();
     }
 }
--- src/LibraryProjectPackage/objects/ChildMember.java
+++ src/LibraryProjectPackage/objects/ChildMember.java
@@ -9,10 +9,15 @@ public class ChildMember extends Member {
     }
     @Override
     public String toString() {
-        return (this.getID() + "\t" +
-                this.getName() + '\n' +
-                "---------------" +
-                "LibraryProjectPackage.objects.Books borrowed: \n" + this.getBorrowed() + "\n");
+        StringBuilder memString = new StringBuilder();
+        memString.append("\n--------------------");
+        memString.append("\nID: ").append(this.getID());
+        memString.append("\nName: ").append(this.getName());
+        memString.append("\nBooks Borrowed:");
+        for (String book: LibraryProjectPackage.Data.getBorrowedBooks(this.getID())){
+            memString.append("\n     ").append(book);
+        }
+        return memString.toString();
     }
 
 }
