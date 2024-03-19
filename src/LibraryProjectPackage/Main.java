671dc242f4dbe202fb8279d79bb8f5bd8e25c44a
Srijita Marick <srijita.marick@ucalgary.ca>
2024-03-18 21:54:10 -0600

    Edited Main.java to take command arguments

--- src/LibraryProjectPackage/Main.java
+++ src/LibraryProjectPackage/Main.java
@@ -1,7 +1,21 @@
 package LibraryProjectPackage;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        Menu.menuLoop();
+        if(args.length > 2){
+            System.err.println("Expected one command line argument for filename to load from");
+        }
+        if(args.length == 1){
+            String filename = args[0];
+            File file = new File(args[0]);
+            if(!file.exists()||!file.canRead()){
+                System.err.println("Cannot load from the file " + filename);
+                System.exit(1);
+            }
+            Menu.menuLoop(file);
+        }
+        Menu.menuLoop(null);
     }
 }
