fbb1ea9068715c04b52e34def6cfe47ee507eabc
Srijita Marick <srijita.marick@ucalgary.ca>
2024-03-19 14:24:26 -0600

    Added output for member type

--- src/LibraryProjectPackage/objects/ChildMember.java
+++ src/LibraryProjectPackage/objects/ChildMember.java
@@ -23,6 +23,7 @@ public class ChildMember extends Member {
         memString.append("\n--------------------");
         memString.append("\nID: ").append(this.getID());
         memString.append("\nName: ").append(this.getName());
+        memString.append("\nMember Type: Child");
         memString.append("\nBooks Borrowed:");
         for (String book: LibraryProjectPackage.Data.getBorrowedBooks(this.getID())){
             memString.append("\n     ").append(book);
